e639d84d4eb989e3aa92650b779053429ea2bcf5
Hichem Abderrahmane Ali Mahdi <hali-mah@2-H-9.42heilbronn.de>
2025-07-28 15:39:31 +0200

    using size_t instead of int in ex01

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: hali-mah <hali-mah@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/22 13:37:48 by hali-mah          #+#    #+#             */
-/*   Updated: 2025/07/28 14:47:15 by hali-mah         ###   ########.fr       */
+/*   Updated: 2025/07/28 15:38:23 by hali-mah         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,11 @@
 #include <string>
 
 template <typename T, typename F>
-void iter(T* array, int l, F function)
+void iter(T* array, size_t l, F function)
 {
-    for (int i = 0; i < l; i++)
+    if (array == nullptr)
+        return ;
+    for (size_t i = 0; i < l; i++)
     {
         function(array[i]);
     }
